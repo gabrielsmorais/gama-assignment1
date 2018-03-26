ecdc362fb34370fcd0a4f97791291ab31efa4a74
Gabriel Morais <gmorais1994@gmail.com>
2018-03-26 01:58:47 -0300

    styles

---
+++ app/js/index.js
@@ -0,0 +1,8 @@
+let $ = document.querySelector.bind(document);
+
+
+window.onload = function() {
+    setTimeout(function() {
+        $("#title").classList.remove("is-loading");
+      }, 100);
+};
